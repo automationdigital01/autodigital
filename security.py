a893e52cad57177089f8d9fcf12456f13cb72efe
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-08-08 14:04:58 +0530

    Update security.py

--- security.py
+++ security.py
@@ -1,4 +1,6 @@
 import requests
 import streamlit as st
-df=requests.get(url="https://technip.sharepoint.com/:x:/r/sites/SecurityNewsDataConsolidation/_layouts/15/Doc.aspx?sourcedoc=%7B6ED3943E-E085-4D2C-B0F8-E76DFC8BCC65%7D&file=CatMainRisk.xlsx&action=default&mobileredirect=true") #without authentication
-st.dataframe(df)
+txt = st.text_area('enter text',)
+st.write(txt)
+#df=requests.get(url="https://technip.sharepoint.com/:x:/r/sites/SecurityNewsDataConsolidation/_layouts/15/Doc.aspx?sourcedoc=%7B6ED3943E-E085-4D2C-B0F8-E76DFC8BCC65%7D&file=CatMainRisk.xlsx&action=default&mobileredirect=true") #without authentication
+#st.dataframe(df)
