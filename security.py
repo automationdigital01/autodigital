77d2e359113f24ca50039dc05f8181f857e962ef
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-08-08 13:28:45 +0530

    Update security.py

--- security.py
+++ security.py
@@ -1,4 +1,4 @@
 import requests
 import streamlit as st
-df=requests.get(url="https://technip.sharepoint.com/:x:/r/sites/SecurityNewsDataConsolidation/Shared%20Documents/General/Project-1%20Config/CatMainRisk.xlsx?d=w6ed3943ee0854d2cb0f8e76dfc8bcc65&csf=1&web=1&e=UVz17t") #without authentication
+df=requests.get(url="https://technip.sharepoint.com/:x:/r/sites/SecurityNewsDataConsolidation/_layouts/15/Doc.aspx?sourcedoc=%7B6ED3943E-E085-4D2C-B0F8-E76DFC8BCC65%7D&file=CatMainRisk.xlsx&action=default&mobileredirect=true") #without authentication
 st.dataframe(df)
