62c13062741816e43a4e940fe0e3e27b92400d78
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-08-08 12:26:26 +0530

    Update security.py

--- security.py
+++ security.py
@@ -1,3 +1,4 @@
 import requests
+import streamlit as st
 df=requests.get(url="https://teams.microsoft.com/l/message/19:718d242f-e372-42bf-a288-6df25ea37531_d7dd5cd3-d65c-455e-bc66-a150adadb027@unq.gbl.spaces/1691477262665?context=%7B%22contextType%22%3A%22chat%22%7D") #without authentication
 st.dataframe(df)
