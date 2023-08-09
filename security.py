6df77ccb25ef996e1c1f77cc0c24fb17f06de4cf
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-08-09 11:39:32 +0530

    Update security.py

--- security.py
+++ security.py
@@ -1,6 +1,7 @@
 import requests
 import streamlit as st
 import pandas as pd
+from office365.sharepoint.client_context import ClientContext
 
 def read_csv(ctx, relative_url, pandas=False):
     response = File.open_binary(ctx, relative_url)
@@ -20,8 +21,11 @@ def read_csv(ctx, relative_url, pandas=False):
     else:
         data = StringIO(s)
         return data
-      
-FILE_URL= "https://technip.sharepoint.com/:x:/r/sites/SecurityNewsDataConsolidation/Shared%20Documents/General/Project-1%20Config/CatMainRisk.xlsx?d=w6ed3943ee0854d2cb0f8e76dfc8bcc65&csf=1&web=1&e=XUxlUY" 
+        
+user_credentials = UserCredential('geetika.pandey@technipenergies.com','zoomer@123')
+ctx = ClientContext('https://technip.sharepoint.com').with_credentials(user_credentials) 
+#https://technip.sharepoint.com/:x:/r/sites/SecurityNewsDataConsolidation/_layouts/15/Doc.aspx?sourcedoc=%7B6ED3943E-E085-4D2C-B0F8-E76DFC8BCC65%7D&file=CatMainRisk.xlsx&action=default&mobileredirect=true
+FILE_URL= "https://technip.sharepoint.com/:x:/r/sites/SecurityNewsDataConsolidation/_layouts/15/Doc.aspx?sourcedoc=%7B6ED3943E-E085-4D2C-B0F8-E76DFC8BCC65%7D&file=CatMainRisk.xlsx&action=default&mobileredirect=true" 
 df= pd.read_csv(read_csv(ctx=ctx, relative_url=FILE_URL, pandas=True), dtype=str, keep_default_na=False) # read master qrd db
 st.dataframe(df)
 #txt = st.text_area('enter text',)
