5602fc21ea830993947c79a956631ee575e3d570
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-08-22 14:33:25 +0530

    Update Security_updated.py

--- Security_updated.py
+++ Security_updated.py
@@ -60,96 +60,102 @@ def ngramprocessing(text,df):
 def main():
     st.title("Security App")
     text = st.text_area('enter text',)
-    # Read the Excel sheet into a DataFrame
-    dfMainRisk = pd.read_excel('data/CatMainRisk.xlsx')
+    if st.button("Submit"):
+        # Read the Excel sheet into a DataFrame
+        dfMainRisk = pd.read_excel('data/CatMainRisk.xlsx')
 
-    # Read the Excel sheet into a DataFrame
-    dfSubRisk = pd.read_excel('data/CatSubRisk.xlsx')
+        # Read the Excel sheet into a DataFrame
+        dfSubRisk = pd.read_excel('data/CatSubRisk.xlsx')
 
-    # Read the Excel sheet into a DataFrame
-    dfThreat = pd.read_excel('data/CatThreatActor.xlsx')
+        # Read the Excel sheet into a DataFrame
+        dfThreat = pd.read_excel('data/CatThreatActor.xlsx')
 
-    # Lemmatize the keywords in the 'Keywords' column of dfThreat
-    dfThreat['Keywords'] = dfThreat['Keywords'].apply(lambda x: ' '.join(lemmatize_tokens(word_tokenize(x))))
+        # Lemmatize the keywords in the 'Keywords' column of dfThreat
+        dfThreat['Keywords'] = dfThreat['Keywords'].apply(lambda x: ' '.join(lemmatize_tokens(word_tokenize(x))))
 
-    #GetMatching Keywords
-    main_matching_keywords =ngramprocessing(text, dfMainRisk)
-    sub_matching_keywords =ngramprocessing(text, dfSubRisk)
-    Threat_matching_keywords =ngramprocessing(text, dfThreat)
+        #GetMatching Keywords
+        main_matching_keywords =ngramprocessing(text, dfMainRisk)
+        sub_matching_keywords =ngramprocessing(text, dfSubRisk)
+        Threat_matching_keywords =ngramprocessing(text, dfThreat)
 
-    #GetMainRisk
-    result_main_risk = main_matching_keywords[['Main risk categories', 'Keywords', 'Matched Tokens']].head(3)
-    if result_main_risk.empty:
-        # Update result_main_risk with data from result_main_sub_risk
-        result_main_risk['Main risk categories']= result_main_sub_risk[['Main risk categories']].copy()
-    st.header("Main Risk")
-    st.dataframe(result_main_risk)
-    csv_main = result_main_risk.to_csv().encode('utf-8')
-    st.download_button(label="Download main risk output",data=csv_main,file_name='main_risk.csv',mime='text/csv',)
-
-
-    #GetSubRisk
-    result_main_sub_risk = sub_matching_keywords[['Main risk categories', 'Keywords', 'Matched Tokens']].head(3)
-    st.header("Main Sub Risk")
-    st.dataframe(result_main_sub_risk)
-    csv_sub = result_main_sub_risk.to_csv().encode('utf-8')
-    st.download_button(label="Download sub risk output",data=csv_sub,file_name='sub_risk.csv',mime='text/csv',)
-
-    # Extract the corresponding "Main risk category" up to top 3 matches
-    result_Threat_risk = Threat_matching_keywords[['Threat actor', 'Keywords', 'Matched Tokens']].head(3)
-    st.header("Threat Actor")
-    st.dataframe(result_Threat_risk)
-    csv_threat = result_Threat_risk.to_csv().encode('utf-8')
-    st.download_button(label="Download Threat Actor output",data=csv_threat,file_name='threat_actor.csv',mime='text/csv',)
+        #GetSubRisk
+        result_main_sub_risk = sub_matching_keywords[['Main risk categories', 'Keywords', 'Matched Tokens']].head(3)
+        st.header("Main Sub Risk")
+        st.dataframe(result_main_sub_risk)
+        csv_sub = result_main_sub_risk.to_csv().encode('utf-8')
+        st.download_button(label="Download sub risk output",data=csv_sub,file_name='sub_risk.csv',mime='text/csv',)
+        
+        
+        
+        
+        #GetMainRisk
+        result_main_risk = main_matching_keywords[['Main risk categories', 'Keywords', 'Matched Tokens']].head(3)
+        if result_main_risk.empty:
+            # Update result_main_risk with data from result_main_sub_risk
+            result_main_risk['Main risk categories']= result_main_sub_risk[['Main risk categories']].copy()
+        st.header("Main Risk")
+        st.dataframe(result_main_risk)
+        csv_main = result_main_risk.to_csv().encode('utf-8')
+        st.download_button(label="Download main risk output",data=csv_main,file_name='main_risk.csv',mime='text/csv',)
 
 
+        
 
-    # Merge the data frames on the common column "Main risk categories"
-    merged_df = pd.merge(result_main_risk,result_main_sub_risk,  on="Main risk categories")
-    st.header("Merged")
-    st.dataframe(merged_df)
-    csv_merged = merged_df.to_csv().encode('utf-8')
-    st.download_button(label="Download merged output",data=csv_merged,file_name='merged.csv',mime='text/csv',)
-
-
-    #Convert to Excel
-    #merged_df.to_excel('FinalMainCatSubCat.xlsx', index=False)
+        # Extract the corresponding "Main risk category" up to top 3 matches
+        result_Threat_risk = Threat_matching_keywords[['Threat actor', 'Keywords', 'Matched Tokens']].head(3)
+        st.header("Threat Actor")
+        st.dataframe(result_Threat_risk)
+        csv_threat = result_Threat_risk.to_csv().encode('utf-8')
+        st.download_button(label="Download Threat Actor output",data=csv_threat,file_name='threat_actor.csv',mime='text/csv',)
 
 
-    if(len(result_Threat_risk)!=0 and len(merged_df)!=0):
-        # Create an empty list to store combined rows
-        combined_rows = []
 
-        # Iterate through rows of dfThreat
-        for idx_threat, row_threat in result_Threat_risk.iterrows():
-            num_repeats = len(merged_df)  # Repeat for each row in merged_df
+        # Merge the data frames on the common column "Main risk categories"
+        merged_df = pd.merge(result_main_risk,result_main_sub_risk,  on="Main risk categories")
+        st.header("Merged")
+        st.dataframe(merged_df)
+        csv_merged = merged_df.to_csv().encode('utf-8')
+        st.download_button(label="Download merged output",data=csv_merged,file_name='merged.csv',mime='text/csv',)
 
-            # Repeat rows from merged_df for the current threat row
-            repeated_merged = pd.concat([merged_df] * num_repeats, ignore_index=True)
 
-            # Add the threat actor and keywords from dfThreat to the repeated_merged
-            repeated_merged["Threat actor"] = [row_threat["Threat actor"]] * len(repeated_merged)
-            repeated_merged["Keywords"] = [row_threat["Keywords"]] * len(repeated_merged)
-            repeated_merged["Matched Tokens"] = [row_threat["Matched Tokens"]] * len(repeated_merged)
+        #Convert to Excel
+        #merged_df.to_excel('FinalMainCatSubCat.xlsx', index=False)
 
-            # Append the combined rows to the list
-            combined_rows.append(repeated_merged)
 
-        # Concatenate all the combined rows into a final dataframe
-        final_df = pd.concat(combined_rows, ignore_index=True)
+        if(len(result_Threat_risk)!=0 and len(merged_df)!=0):
+            # Create an empty list to store combined rows
+            combined_rows = []
 
-    if(len(result_Threat_risk)==0):
-        final_df =merged_df
+            # Iterate through rows of dfThreat
+            for idx_threat, row_threat in result_Threat_risk.iterrows():
+                num_repeats = len(merged_df)  # Repeat for each row in merged_df
 
-    if(len(merged_df)==0):
-        final_df =result_Threat_risk
+                # Repeat rows from merged_df for the current threat row
+                repeated_merged = pd.concat([merged_df] * num_repeats, ignore_index=True)
 
-    st.header("Final")    
-    st.dataframe(final_df)
+                # Add the threat actor and keywords from dfThreat to the repeated_merged
+                repeated_merged["Threat actor"] = [row_threat["Threat actor"]] * len(repeated_merged)
+                repeated_merged["Keywords"] = [row_threat["Keywords"]] * len(repeated_merged)
+                repeated_merged["Matched Tokens"] = [row_threat["Matched Tokens"]] * len(repeated_merged)
 
-    csv_final = final_df.to_csv().encode('utf-8')
-    st.download_button(label="Download final output",data=csv_final,file_name='final.csv',mime='text/csv',)
-    #print(final_df)
+                # Append the combined rows to the list
+                combined_rows.append(repeated_merged)
+
+            # Concatenate all the combined rows into a final dataframe
+            final_df = pd.concat(combined_rows, ignore_index=True)
+
+        if(len(result_Threat_risk)==0):
+            final_df =merged_df
+
+        if(len(merged_df)==0):
+            final_df =result_Threat_risk
+
+        st.header("Final")    
+        st.dataframe(final_df)
+
+        csv_final = final_df.to_csv().encode('utf-8')
+        st.download_button(label="Download final output",data=csv_final,file_name='final.csv',mime='text/csv',)
+        #print(final_df)
 
 if __name__ == "__main__":
     nltk.download('punkt')       # Download the punkt tokenizer models (if not already downloaded)
