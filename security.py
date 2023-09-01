61488250a5096bba3fe61ca00098e24cacf8adf6
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-09-01 20:12:27 +0530

    Update security.py

--- security.py
+++ security.py
@@ -336,7 +336,7 @@ def main():
         #with zipfile.ZipFile(model_file, "r") as zip_ref:
          #   zip_ref.extractall("en_core_web_sm")
 
-        model_path = "./en_core_web_sm-3.5.0"
+        model_path = "en_core_web_sm/en_core_web_sm-3.5.0"
         nlp = spacy.load(model_path)
     
     if st.button("Submit"):
